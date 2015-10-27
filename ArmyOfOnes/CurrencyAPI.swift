6fdd261607e0cf3cce72c13b3bbe5a28fae43851
Giorgia <giorgia.marenda@gmail.com>
2015-10-27 11:29:54 -0400

    Start implementation of view controller

--- ArmyOfOnes/CurrencyAPI.swift
+++ ArmyOfOnes/CurrencyAPI.swift
@@ -11,29 +11,32 @@ import Alamofire
 import ObjectMapper
 import AlamofireObjectMapper
 
-enum Currency: Int {
-    case US, UK, EU, JP, BS
+enum Currency: String {
+    case US = "USD"
+    case UK = "GBP"
+    case EU = "EUR"
+    case JP = "JPY"
+    case BZ = "BRL"
     
-    var standardDesc: String {
-         switch self {
-         case .US: return "USD"
-         case .UK: return "GBP"
-         case .EU: return "EUR"
-         case .JP: return "JPY"
-         case .BS: return "BRL"
-        }
-    }
     var flag: String {
         switch self {
         case .US: return "🇱🇷"
         case .UK: return "🇬🇧"
         case .EU: return "🇪🇺"
         case .JP: return "🇯🇵"
-        case .BS: return "🇧🇷"
+        case .BZ: return "🇧🇷"
         }
     }
     
-    static let allValues = [US, UK, EU, JP, BS]
+    var symbol: String {
+        switch self {
+        case .US: return "$"
+        case .UK: return "£"
+        case .EU: return "€"
+        case .JP: return "¥"
+        case .BZ: return "R$"
+        }
+    }
 }
 
 let baseURL = "https://currency-api.appspot.com"
@@ -44,7 +47,7 @@ class CurrencyAPI {
      Performs the GET request at Currency API
      */
     class func convertion(from source: Currency, to target: Currency, amount: Int, completion: (Conversion?, ErrorType?) -> Void) {
-        let currencyPath = baseURL + "/api/\(source.standardDesc)/\(target.standardDesc).json"
+        let currencyPath = baseURL + "/api/\(source.rawValue)/\(target.rawValue).json"
        
         Alamofire.request(.GET, currencyPath, parameters: ["amount": amount])
             .responseObject { (conversion: Conversion?, error) -> Void in
@@ -58,11 +61,11 @@ class CurrencyAPI {
 }
 
 class Conversion: Mappable {
+    
     var amount:     Double?
     var rate:       Double?
     var source:     String?
     var target:     String?
-    
     var success:    Bool = false
     var message:    String?
     
--- ArmyOfOnes/ViewController.swift
+++ ArmyOfOnes/ViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let reuseTableViewCellIdentifier = "Cell"
+
+class CurrencyCell: UITableViewCell {
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var symbolLabel: UILabel!
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    let currencies  = [Currency.UK, Currency.EU, Currency.JP, Currency.BZ]
+    var conversions: [Conversion?]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        conversions = [Conversion?](count:currencies.count, repeatedValue: nil)
+        reloadAllConversions(1)
+    }
+    
+    func loadUSDConversion(target: Currency, amount: Int) {
+        CurrencyAPI.convertion(from: .US, to: target, amount: amount, completion: { (conversion, error) -> Void in
+            guard
+                let index = self.currencies.indexOf(target) else { return }
+            self.conversions?[index] = conversion
+            self.tableView.reloadRowsAtIndexPaths([NSIndexPath(forItem: index, inSection: 0)], withRowAnimation: UITableViewRowAnimation.None)
+        })
+    }
+    
+    func reloadAllConversions(amount: Int) {
+        for c in currencies {
+            loadUSDConversion(c, amount: amount)
+        }
     }
 
+    @IBAction func go() {
+        reloadAllConversions(2)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencies.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseTableViewCellIdentifier, forIndexPath: indexPath)
+       
+        guard
+            let currencyCell    = cell as? CurrencyCell,
+            let conversion      = conversions?[indexPath.row],
+            let target          = conversion.target,
+            let amount          = conversion.amount,
+            let currency        = Currency(rawValue: target)
+            else { return cell }
+        
+        currencyCell.symbolLabel.text = currency.flag
+        currencyCell.amountLabel.text = "\(amount) \(currency.symbol)"
+    
+        return currencyCell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
